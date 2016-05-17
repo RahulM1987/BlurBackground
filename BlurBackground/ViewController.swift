449864b7324992deac4af5bac1f21abe541fa928
RahulM1987 <rahulmorade@winjit.local>
2016-05-17 11:30:59 +0530

    Initial Commit

---
+++ BlurBackground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlurBackground
+//
+//  Created by Rahul Morade on 17/05/16.
+//  Copyright © 2016 Rahul Morade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
